ae15c17d46185a990166a4eaf0fdd9e506bd7771
Tyler Critchfield <trcritchfield@gmail.com>
2019-06-05 16:14:50 -0700

    updating plotter.py

--- Simulation/Plotter.py
+++ Simulation/Plotter.py
@@ -1,13 +1,24 @@
+import matplotlib.pyplot as plt
+
 class Plotter:
-	'Class used to make plots of simulation results'
+	# 'Class used to make plots of simulation results'
 
 	# class variables go here:
-	xlabel = "Time (s)"
-	ylabel = "Range (m)"
+	xlabel = "Time [s]"
+	ylabel = "Range [m]"
 	title = "Example Drone"
 
 	# methods go here:
-	def __init__(self, axistitle):
+	def __init__(self, axistitle,xlabel,ylabel):
 		self.title = axistitle
+		self.xlabel = xlabel
+		self.ylabel = ylabel
+
+	def plot(self,x,y)
+		plt.plot(x,y)
+		plt.xlabel(self.xlabel)
+		plt.ylabel(self.ylabel)
+		plt.title(self.title)
+		plt.show()
 
 print("Successfully imported `Plotter.py`")
