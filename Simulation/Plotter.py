79381f19c87ca6d9fb79916c40321971510640e1
rymanderson <rymanderson@gmail.com>
2019-06-05 15:20:42 -0600

    Started creating class .py files

---
+++ Simulation/Plotter.py
@@ -0,0 +1,13 @@
+class Plotter:
+	'Class used to make plots of simulation results'
+
+	# class variables go here:
+	xlabel = "Time (s)"
+	ylabel = "Range (m)"
+	title = "Example Drone"
+
+	# methods go here:
+	def __init__(self, axistitle):
+		self.title = axistitle
+
+print("Successfully imported `Plotter.py`")
---
+++ Simulation/Power.py
@@ -0,0 +1,12 @@
+class Power:
+        'Class used to predict the drone\'s power requirement'
+
+        # class variables go here:
+        
+
+        # methods go here:
+        def __init__(self):
+                
+
+print("Successfully imported `Plotter.py`")
+
