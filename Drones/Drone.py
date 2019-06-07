c5b868fd336af06dcdc0bda7d124c40fdd516bef
tylercritchfield <trcritchfield@gmail.com>
2019-06-07 16:37:04 -0700

    almost done with Drone class, still need to find params in subdirectory

--- Drones/Drone.py
+++ Drones/Drone.py
@@ -1,12 +1,33 @@
 import csv
 
 class Drone:
-	'Class used to store key drone characteristics'
+	# 'Class used to store key drone characteristics'
 
 	# class variables go here:
-	name        = "Mavic2" # must match the .param file in the params/ directory
-    wingtype    = "rotary"
-	battery     = {'type':'LiPo', 'capacity':0.0, 'voltage':0.0}
+
+	# name        = "Mavic2" # must match the .param file in the params/ directory
+    # wingtype    = "rotary"
+	# battery     = {'type':'LiPo', 'capacity':0.0, 'voltage':0.0}
+
+	name = "Mavic2"
+	params = {
+			'wingtype':None,
+			'TOW':None,
+			'max_speed':None,
+			'max_alt':None,
+			'max_t':None,
+			'max_t_hover':None,
+			'max_tilt':None,
+			'min_temp':None,
+			'max_temp':None,
+			'power_rating':None,	
+			'batt_type':None,
+			'batt_capacity':None,
+			'batt_voltage':None,
+			'batt_cells':None,
+			'batt_energy':None,
+			'batt_mass':None
+			}
 
 	# methods go here:
 	def __init__(self, name):
@@ -14,25 +35,39 @@ class Drone:
 
 	def getparams(self):
 		# find name.param in the params/ directory
-		paramlist = open("paramlist.param","r")
-		paramfile = open(self.name + ".param", "r") #open file with read privilege
+		with open("paramlist.param","r") as paramlist:
+			with open(self.name + ".param", "r") as paramfile:
+				paramlist = paramlist.readlines()
+				paramfile = paramfile.readlines() #open file with read privilege
 
-		#read .txt file
-		with f as inf:
-     		reader = csv.reader(inf, delimiter=" ")
-     		specs = list(zip(*reader))[0]
+				specs = [] #initialize lists
+				values = []
 
-		for param in paramlist
-			if "param" in specs:
-				#create variable with that name
-				#exec("%s = %d" % (param,param_value)) - frowned upon according to Stack Exchange
-				#could instead reference dictionaries
-				#need to creat list file or dictionary
+				for line in paramfile: #separates columns from .param file
+					parts = line.split()
+					specs.append(parts[0])
+					values.append(parts[1])
+				for line in paramlist:
+					line = line.strip() #gets rid of quotation marks when comparing strings
+					for spec in specs:
+						if line == spec:
+							if spec == "wingtype" or spec == "batt_type":
+								self.params[spec] = values[specs.index(spec)]
+							else:
+								self.params[spec] = float(values[specs.index(spec)])
 
-			# for line in paramfile
-			# 	values = line.split()
-			# 	print(values[0], " - ", values[1])
-
-		return 0
+				# for param in paramfile: #separates columns from .param file
+				# 	parts = param.split()
+				# 	spec = parts[0]
+				# 	value = parts[1]
+				# 	specs.append(spec)
+				# 	values.append(value)
+				# 	for line in paramlist:
+				# 		line = line.strip() #gets rid of quotation marks when comparing strings
+				# 		if line == spec:
+				# 			if spec == "wingtype" or spec == "batt_type":
+				# 				self.params[spec] = value
+				# 			else:
+				# 				self.params[spec] = float(value)
 
 print("Successfully imported `Drone.py`")
