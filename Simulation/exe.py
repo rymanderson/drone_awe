3b722dc770908c66d3586e79f78852a9417b70bc
tylercritchfield <trcritchfield@gmail.com>
2019-06-17 13:57:11 -0700

    moved Classes.py, updated exe.py

--- Simulation/exe.py
+++ Simulation/exe.py
@@ -1,59 +1,55 @@
 import awesomeModelName_importer
 
-Dronename = 'dji-Mavic2'
-SoH = 90
+# Dronename = 'dji-Mavic2'
+# SoH = 90
 
-with open('textfile.txt','r') as filename
+with open('textfile.txt','r') as fp
+    drone = readline()
+    if drone == true:
+        dronename = fp.readline()
+        drone = Drone(dronename)
+    else:
+        raise Exception('Must specify drone name')
 
-# from google doc:
-Drone = readline
-If drone==true:
-	Dronename = readline
-	…
-	drone = Drone(Dronename…)
-Else
-	error(‘Must specify drone name’)
+    # instantiate battery
+    battery = Battery(drone)
 
-# instantiate battery
-battery = Battery(drone)
+    #get class initialization info
+        fp.readline()
+        if rain == true:
+            Dropsize = readline
+            WVC = readline
+            # …
+            rain = Rain(dropsize,WVC,...)
+        else:
+            for i = 1:rainlength:
+                fp.readline()
 
-#get class initialization info
-Open settings.txt
-	Readline…
-	If rain==true:
-		Dropsize = readline
-		WVC = readline
-		…
-		rain = Rain(dropsize,WVC,...)
-	Else
-		For i=1:rainlength:
-			Readline
-
-	#simulation variables
-	Timestep = readline
-	…
-	#plot settings
-	##time plots
-	Totalpower	true
-	Rainintensity false
-	…
-    # VVV In text file itself VVV
-    # ##time invariant plots: select two parameters from list:
-	# ### range payload endurance …
-	# #### example:
-	# #### range	payload
-	# plot range payload
-	# ^^^ 		         ^^^
-	while eof == false:
-	next = readline()
-	if next==plot:
-		plotx.push(readline())
-		ploty.push(readline())
-		Else:
-			error(“invalid plot syntax”)
+        #simulation variables
+        Timestep = fp.readline()
+        …
+        #plot settings
+        ##time plots
+        Totalpower	true
+        Rainintensity false
+        …
+        # VVV In text file itself VVV
+        # ##time invariant plots: select two parameters from list:
+        # ### range payload endurance …
+        # #### example:
+        # #### range	payload
+        # plot range payload
+        # ^^^ 		         ^^^
+        while eof == false:
+        nextline = readline()
+        if nextline == plot:
+            plotx.push(fp.readline())
+            ploty.push(fp.readline())
+        else:
+            raise Exception('Invalid plot syntax')
 
 simulation = Simulation(drone,weather,power,...timestep,...)
-simulation.run
+simulation.run()
 
 plotter = Plotter(inputs….)
-plotter.plot(
+plotter.plot()
