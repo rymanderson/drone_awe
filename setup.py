db78a78de5e5b654721efacb04d50c59a6e5a1e6
rymanderson <rymanderson@gmail.com>
2019-07-30 10:01:00 -0600

    First PyPI release v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A versatile modeling package for drone performance in various weather environments.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    download_url="https://github.com/rymanderson/Drone-Models.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
